6037dd4aabdc161fecab5a53638df6143b5e2dbc
Sean Doyle <sean.p.doyle24@gmail.com>
2020-03-05 20:25:20 -0500

    extract form partial

--- test/system/author_edits_page_content_test.rb
+++ test/system/author_edits_page_content_test.rb
@@ -18,19 +18,13 @@ class AuthorEditsPageContentTest < ApplicationSystemTestCase
     visit page_path(page.slug)
     click_on login_button
     within("h1", text: page.hero_text) do
-      expand translate("pages.show.edit")
-      fill_in translate("helpers.label.page.hero_text"), with: new_hero_text
-      click_on translate("helpers.submit.page.update")
+      edit_content(:hero_text, new_hero_text)
     end
     within("h1", text: page.headline_text) do
-      expand translate("pages.show.edit")
-      fill_in translate("helpers.label.page.headline_text"), with: new_headline_text
-      click_on translate("helpers.submit.page.update")
+      edit_content(:headline_text, new_headline_text)
     end
     within("div", text: page.body_text) do
-      expand translate("pages.show.edit")
-      fill_in translate("helpers.label.page.body_text"), with: new_body_text
-      click_on translate("helpers.submit.page.update")
+      edit_content(:body_text, new_body_text)
     end
 
     assert_selector "h1", text: new_hero_text
@@ -38,11 +32,17 @@ class AuthorEditsPageContentTest < ApplicationSystemTestCase
     assert_selector "div", text: new_body_text
   end
 
+  def login_button
+    translate("helpers.submit.session.create")
+  end
+
   def expand(label)
     find("summary", text: label).click
   end
 
-  def login_button
-    translate("helpers.submit.session.create")
+  def edit_content(attribute, value)
+    expand translate("pages.show.edit")
+    fill_in translate(attribute, scope: "helpers.label.page"), with: value
+    click_on translate("helpers.submit.page.update")
   end
 end
