1fb45f3bddf3be7030e4e9419e9606cd2f5d7095
Sean Doyle <sean.p.doyle24@gmail.com>
2020-03-28 17:11:24 -0400

    Fetch edit form remotely
    variants: https://guides.rubyonrails.org/4_1_release_notes.html#action-pack-variants
    
    attributes only available over XHR

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   before_action do
     Current.admin_id = session[:admin_id]
   end
+
+  before_action do
+    if request.get? && request.xml_http_request?
+      request.variant = [:fragment]
+    end
+  end
 end
