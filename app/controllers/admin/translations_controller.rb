73603d6d2826a3967f67450c4f6d7c9e582608f1
Sean Doyle <sean.p.doyle24@gmail.com>
2020-10-02 02:49:33 -0400

    Limit Edits to an Authenticated Admin

---
+++ app/controllers/admin/translations_controller.rb
@@ -0,0 +1,19 @@
+module Admin
+  class TranslationsController < ApplicationController
+    def create
+      translation = Translation.new(translation_params)
+
+      if translation.save
+        redirect_back fallback_location: root_url
+      else
+        head :unprocessable_entity
+      end
+    end
+
+    private
+
+    def translation_params
+      params.require(:translation).permit(:locale, :key, :value)
+    end
+  end
+end
--- test/controllers/marketings_controller_test.rb
+++ test/controllers/marketings_controller_test.rb
@@ -3,7 +3,18 @@ require "test_helper"
 class MarketingsControllerTest < ActionDispatch::IntegrationTest
   include ActionView::Helpers::TranslationHelper
 
-  test "#index renders editors for copy" do
+  test "#index renders marketing copy" do
+    get root_path
+
+    assert_response :success
+    with_options scope: [:marketings, :index, :hero] do |i18n|
+      assert_select "title", text: i18n.translate(:title)
+      assert_select "h1", text: i18n.translate(:title)
+    end
+  end
+
+  test "#index renders editors for copy when authenticated" do
+    post sessions_path
     get root_path
 
     assert_response :success
